530e93b477636ef704ec447acd13a28f7acf5b9e
IrinaT <irina.tiora@isa.utm.md>
2023-10-08 14:49:43 +0300

    Add lessons project structure

--- lessons/cmd/main.go
+++ lessons/cmd/main.go
@@ -1,1 +1,20 @@
-package cmd
+package main
+
+import (
+	"github.com/EliriaT/school-api/lessons/internal/controller/course"
+	httphandler "github.com/EliriaT/school-api/lessons/internal/handler/http"
+	"github.com/EliriaT/school-api/lessons/internal/repository/memory"
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Starting the rating service")
+	repo := memory.New()
+	ctrl := course.New(repo)
+	h := httphandler.New(ctrl)
+	http.Handle("/course", http.HandlerFunc(h.Handle))
+	if err := http.ListenAndServe(":8082", nil); err != nil {
+		panic(err)
+	}
+}
